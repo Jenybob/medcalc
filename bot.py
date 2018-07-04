88dca46fb9ac0c9bd4b8b017b181005f17674be1
Jenybob <zhenyapinahin@gmail.com>
2018-07-04 21:32:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,9 @@
 import telepot
-from telepot.loop import MessageLoop
 
-TOKEN = '583171379:AAGMemuXxpQB3u_KorK7xZ_uYokQ54k33SQ'
-bot = telepot.Bot(TOKEN)
+from pprint import pprint
 
-def handle(msg):
-    """ Process request like '3+2' """
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    text = msg["text"]
-    try:
-        answer = eval(text)
-    except:
-        answer = "Can't calculate :("
-    bot.sendMessage(chat_id, "answer: {}".format(answer))
+bot = telepot.Bot('583171379:AAGMemuXxpQB3u_KorK7xZ_uYokQ54k33SQ')
 
+response = bot.getUpdates()
 
-MessageLoop(bot, handle).run_as_thread()
-
-# Keep the program running.
-while True:
-    n = input('To stop enter "stop":')
-    if n.strip() == 'stop':
-        break
+pprint(response)
